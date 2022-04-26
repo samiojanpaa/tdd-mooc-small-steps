bc7a527c29e697c19962a5909f7a013d3cf9480d
Sami Ojanpää <sami.ojanpaa@softacon.fi>
2022-04-26 08:29:01 +0300

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -80,7 +80,7 @@ function createApp(database) {
     return date.dayOfWeek === 1;
   }
 
-  function isHoliday(date, date2) {
+  function isHoliday(date) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = Temporal.PlainDate.from(row.holiday);
